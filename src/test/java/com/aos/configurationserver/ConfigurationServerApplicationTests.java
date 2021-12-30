f8771aad0c4bee96066b20337035a90fc6ca9dfb
emmy202 <hitimanaemm10@gmail>
2021-12-30 14:43:57 +0200

    Initial commit

---
+++ src/test/java/com/aos/configurationserver/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aos.configurationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
